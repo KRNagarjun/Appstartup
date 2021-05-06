3b238bc8c7ae87bc6f3fd0c89f5ddb6b13d4a446
KRNagarjun <kr.nagarjun@ltts.com>
2021-05-06 11:57:05 +0530

    APP STARTUP DEMO

---
+++ Filters/RequestSetOptionsStartupFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppStartupClassDemo.Filters
+{
+    public class RequestSetOptionsStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return builder =>
+            {
+                builder.UseMiddleware<RequestSetOptionsMiddleware>();
+                next(builder);
+            };
+        }
+    }
+}
